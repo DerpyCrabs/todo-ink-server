acf637ff3ca70de363e073d807082a48b80bcda2
vgoma-digimatics <vgoma@digimatics.ru>
2021-03-14 19:11:55 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+const PORT = 4200
+
+app.get('/', (req, res) => res.send('Express + TypeScript Server'))
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+})
